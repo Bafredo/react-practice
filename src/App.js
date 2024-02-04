09231258a0439935d74f60ac1d455403df71833b
komenaustine <komenaustine@gmail.com>
2024-02-04 15:56:04 +0300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import './App.css';
 import React from 'react';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
-      <div className="navBar">
-        <div className='logo'>Freddy</div>
-        <div className='search'>
-          <form>
-            <input className='searchBox' type ='text' placeholder='Search'/>
-            <input type='submit' value='search'/>
-          </form>
-          </div>
-        <div className='cart'>Cart</div>
-      </div>
+      <Header />
     </div>
   );
 }
