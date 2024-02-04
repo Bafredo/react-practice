292a47a408e303b3de342494cb41e8f326660f52
Bafredo <muyomafredrick@gmail.com>
2024-02-04 20:18:07 +0300

    Data fetched and content displayed minus images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import React from 'react';
 import Header from './components/header';
+import { Content } from './components/content';
+
+const button = document.getElementById('btn');
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='content'>
+        <Content />
+      </div>
+      
     </div>
   );
 }
